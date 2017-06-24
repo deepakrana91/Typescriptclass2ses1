ab7fd2f0f6f550f137f85fc942328d8e80bb7eb2
deepakrana91 <deepakrana91@live.com>
2017-06-24 20:45:51 +0530

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,5 @@
+//This is a Arrow or lamda function which will take 2 arguments one is string and other one as number.
+var display = function (Arg1, Arg2) { return (Arg1 + Arg2); };
+var Getter = display("Ferrari", 100);
+//output would be display in the console
+console.log(Getter);
---
+++ Display.ts
@@ -0,0 +1,9 @@
+
+
+//This is a Arrow or lamda function which will take 2 arguments one is string and other one as number.
+
+let display = (Arg1:string,Arg2:number):string => {return(Arg1+Arg2)}
+
+let Getter=display("Ferrari",100);
+
+console.log(Getter);
